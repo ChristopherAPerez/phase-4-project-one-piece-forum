52b3823855ce3125e26332e694e82929a587890d
Christopher Perez <ChrisAmadeusPerez@gmail.com>
2023-01-22 03:22:58 -0500

    model User Update Associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    has_many :comments
+    has_many :forums, through: :comments
+
+    validates :username, presence: true, uniqueness: true
 end
