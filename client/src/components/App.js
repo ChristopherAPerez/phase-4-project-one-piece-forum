fb19173098602c238b91fa05eda413c45794cb0a
Christopher Perez <ChrisAmadeusPerez@gmail.com>
2023-01-22 04:58:12 -0500

    App Update

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Header from "./Header"
+import NavBar from "./NavBar"
+
+import LoggedIn from "./LoggedIn"
+import Profile from "../pages/Profile"
+
+import LoggedOut from "./LoggedOut"
+import LoginForm from "../pages/LoginForm"
+import SignUpForm from "../pages/SignUpForm";
+
+
+
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // auto-login
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {user ? (
+        <>
+          <Header />
+          <NavBar />
+        </>
+      ) : (
+        <Header />
+      )}
+
+      <br></br>
+      <br></br>
+
+      <main>
+        {user ? (
+
+          <Routes>
+            <Route path="/profile" element={<Profile user={user} setUser={setUser} />}>
+            </Route>
+            {/* <Route path="/upload" element={<Upload user={user} setUser={setUser} />}>
+            </Route> */}
+            <Route path="/" element={<LoggedIn user={user} />}>
+            </Route>
+          </Routes>
+
+        ) : (
+
+          <Routes>
+            <Route path="/signup" element={<SignUpForm setUser={setUser} />}>
+            </Route>
+            <Route path="/login" element={<LoginForm setUser={setUser} />}>
+            </Route>
+            <Route path="/" element={<LoggedOut user={user} />}>
+            </Route>
+          </Routes>
+
+        )}
+      </main>
+
     </div>
   );
 }
